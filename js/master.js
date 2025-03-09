050ecc3ad8e1bf72114b02ab8d06698e3805a0ca
Ahmed Farid <ahmed.super997@gmail.com>
2025-03-09 11:52:56 +0200

    Edit on accessory ship date. and Add columns for Gender SeasonCode and DesCountry

--- js/master.js
+++ js/master.js
@@ -86,9 +86,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const RefCode = `${row["Ref#"]}-${row["Color"]}`;
       const LP = `${row["Style"]}-${row["Color"]}-${row["Label"]}`;
       const ACCShipDate = generateACCShipDate(
-        row["Year"].toString().trim(),
-        row["Ship Date"].toString().trim()
+        row["Original ETD"].toString().trim()
       );
+      const seasonCode = generateSeasonCode(row["Season"], row["Year"]);
+      const LPDes = `${String(row["Account"]).trim()}-${String(
+        row["Label"]
+      ).trim()}`;
+      const desCountry = generateDesCountry(LPDes);
       if (
         !row["Size Configuration"] ||
         !row["Pack Ratio"] ||
@@ -101,6 +105,8 @@ document.addEventListener("DOMContentLoaded", () => {
           RefCode,
           LP,
           ACCShipDate,
+          seasonCode,
+          desCountry,
           Error: "Missing required fields",
         });
         return;
@@ -154,6 +160,8 @@ document.addEventListener("DOMContentLoaded", () => {
           RefCode,
           LP,
           ACCShipDate,
+          seasonCode,
+          desCountry,
         });
       });
 
@@ -165,6 +173,8 @@ document.addEventListener("DOMContentLoaded", () => {
         RefCode,
         LP,
         ACCShipDate,
+        seasonCode,
+        desCountry,
       });
     });
 
@@ -188,6 +198,8 @@ document.addEventListener("DOMContentLoaded", () => {
       "RefCode",
       "LP",
       "ACCShipDate",
+      "seasonCode",
+      "desCountry",
       "orderingNumber",
       "bySize",
       "finalQTY",
@@ -323,10 +335,131 @@ document.addEventListener("DOMContentLoaded", () => {
     XLSX.writeFile(wb, "SizeConfiguration.xlsx");
   }
 
-  function generateACCShipDate(season, shipDate) {
-    const date = new Date(shipDate);
+  function generateACCShipDate(OriginalDate) {
+    const date = new Date(OriginalDate);
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const seasonCode = (parseInt(season) % 100) * 2;
-    return `${month}${seasonCode.toString().padStart(2, "0")}`;
+    const yearCode = (parseInt(date.getFullYear()) % 100) * 2;
+    return `${month}${yearCode.toString().padStart(2, "0")}`;
+  }
+
+  function generateSeasonCode(season, year) {
+    const date = new Date(year);
+    const yearCode = parseInt(date.getFullYear()) % 100;
+    const seasonCode = {
+      s: 1,
+      m: 2,
+      f: 3,
+      h: 4,
+    };
+    return `${seasonCode[season.toLowerCase()]}${yearCode}`;
+  }
+
+  function generateDesCountry(LPDes) {
+    const data = {
+      "A109M-AC": "USA-Stock",
+      "A109M-AD": "USA-Stock",
+      "A109M-AH": "USA-Stock",
+      "A109M-AM": "USA-Stock",
+      "A109M-AN": "USA-Stock",
+      "A311M-X0": "Global-India",
+      "A313M-QO": "Global-Dubai",
+      "A746M-AL": "Global-Israel",
+      "C104M-G0": "Global-Thailand",
+      "C104M-G8": "Global-Thailand",
+      "C104M-G9": "Global-Thailand",
+      "C104M-H0": "Global-Thailand",
+      "D528M-DS": "USA-Stock",
+      "D552M-DL": "USA-Stock",
+      "H040M-": "USA-Stock",
+      "H040M-EC": "USA-Stock",
+      "H040M-ED": "USA-Stock",
+      "H040M-EE": "USA-Stock",
+      "H040M-EJ": "USA-Stock",
+      "H040M-EM": "USA-Stock",
+      "H040M-EN": "USA-Stock",
+      "H040M-IP": "USA-Stock",
+      "H040M-NH": "USA-Stock",
+      "H040M-P2": "USA-Stock",
+      "H040M-WH": "USA-Stock",
+      "H494M-L5": "Global-EUROPE",
+      "H494M-LQ": "Global-EUROPE",
+      "H494M-LR": "Global-EUROPE",
+      "H494M-LV": "Global-EUROPE",
+      "H494M-LW": "Global-EUROPE",
+      "H494M-LY": "Global-EUROPE",
+      "H494M-LZ": "Global-EUROPE",
+      "H718M-KL": "Global-Korea",
+      "H718M-KM": "Global-Korea",
+      "H718M-KS": "Global-Korea",
+      "H718M-KX": "Global-Korea",
+      "I201M-KO": "Global-Mexico",
+      "I201M-MH": "Global-Mexico",
+      "I201M-MK": "Global-Mexico",
+      "I201M-MO": "Global-Mexico",
+      "I201M-MV": "Global-Mexico",
+      "I644M-IA": "Global-Panama",
+      "I644M-NL": "Global-Panama",
+      "I867M-XL": "Global-Chile",
+      "I952M-O9": "Global-Peru",
+      "J121M-JC": "USA-Stock",
+      "K812M-KO": "USA-Stock",
+      "K816M-1Z": "USA-Stock",
+      "K816M-2Z": "USA-Stock",
+      "K816M-3Z": "USA-Stock",
+      "K816M-4Z": "USA-Stock",
+      "K816M-EK": "USA-Stock",
+      "K816M-H6": "USA-Stock",
+      "K816M-Z5": "USA-Stock",
+      "K816M-Z7": "USA-Stock",
+      "L004M-2X": "Costco Australia",
+      "S068M-EY": "USA-SAM'S",
+      "S068M-EZ": "USA-SAM'S",
+      "S068M-SM": "USA-SAM'S",
+      "S068M-SQ": "USA-SAM'S",
+      "S068M-SU": "USA-SAM'S",
+      "S960M-F5": "Global-Malaysia",
+      "S974M-SY": "Global-South Africa",
+      "T059M-TZ": "USA-Target",
+      "T082M-DE": "USA-Target",
+      "T082M-DF": "USA-Target",
+      "T082M-DH": "USA-Target",
+      "T082M-DM": "USA-Target",
+      "T082M-EI": "USA-Target",
+      "T082M-EL": "USA-Target",
+      "T082M-EO": "USA-Target",
+      "T082M-FO": "USA-Target",
+      "T082M-GA": "USA-Target",
+      "T082M-GC": "USA-Target",
+      "T082M-GD": "USA-Target",
+      "T082M-GE": "USA-Target",
+      "T082M-GF": "USA-Target",
+      "W098M-0W": "USA-WALMART",
+      "W098M-W1": "USA-WALMART",
+      "W098M-W2": "USA-WALMART",
+      "W098M-W3": "USA-WALMART",
+      "W098M-WM": "USA-WALMART",
+      "W098M-WQ": "USA-WALMART",
+      "W098M-WR": "USA-WALMART",
+      "W113M-HA": "USA-WALMART",
+      "W113M-HC": "USA-WALMART",
+      "W113M-HD": "USA-WALMART",
+      "W113M-HE": "USA-WALMART",
+      "W113M-HF": "USA-WALMART",
+      "W113M-HH": "USA-WALMART",
+      "W113M-HI": "USA-WALMART",
+      "W113M-HJ": "USA-WALMART",
+      "W113M-HK": "USA-WALMART",
+      "W113M-HL": "USA-WALMART",
+      "W113M-HN": "USA-WALMART",
+      "W113M-HO": "USA-WALMART",
+      "W113M-HP": "USA-WALMART",
+      "W113M-HR": "USA-WALMART",
+      "W846M-CN": "Global-Canada STOCK",
+      "W846M-FZ": "Global-CANADA FGL SPORTS",
+      "W846M-WN": "Global-CANADA WINNERS",
+      "W847M-WO": "USA-WALMART",
+      "Z090M-SV": "Global-Panama ZafariI",
+    };
+    return data[LPDes];
   }
 });
